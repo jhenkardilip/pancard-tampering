9da0b467eb3f9d813d39d5557c21a08842eae7d1
jhenkardilip <160463432+jhenkardilip@users.noreply.github.com>
2024-09-04 05:20:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,4 @@
+from app import app                          ##This line imports the app object from the app module
+
+if __name__ == '__main__':                   ##This condition checks whether the script is being run directly 
+   app.run()
